d067c5f58b94f8d559abab612bcde0ef6f121128
Ayantu Ayana <al1272@live.mdx.ac.uk>
2025-11-27 01:14:50 +0400

    Remove week 8 folder

--- week 8/app/services/__init__.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-# Create __init__.py files if they don't exist
-Path("app/__init__.py").touch()
-Path("app/data/__init__.py").touch()
-Path("app/services/__init__.py").touch()
